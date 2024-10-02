b68f0f4cef657a68ff31ba26458d1188217745f2
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-03 00:00:07 +0530

    BasicJava

---
+++ Basic1.java
@@ -0,0 +1,21 @@
+public class Basic1 {
+    public static void main(String[] args){
+
+        //* --------------------------print in one line-->**print
+        System.out.print("one");
+        System.out.print("two");
+        System.out.println("three");
+        
+        
+        
+        //* --------------------------print in one different line-->**println
+        System.out.println("one");
+        System.out.println("two");
+        System.out.println("three");
+
+
+        //*-----------------------------print
+
+
+    }
+}
