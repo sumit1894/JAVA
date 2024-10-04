c15e10cff862dc5e124efa31a2b7f45db44e6e76
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-04 20:26:05 +0530

    3-Task-Pattern

---
+++ Task_Pattern_3.java
@@ -0,0 +1,23 @@
+public class Task_Pattern_3 {
+    public static void main(String args[]) {
+
+        // ! Show the following patterns using single print statement:
+        // *RightHalfPyramid
+        System.out.println("*\n**\n***\n****\n*****");
+
+
+        System.out.println("-------------------------------------------------\n");
+
+
+        // *ReverseHalfPyramid
+        System.out.println("*****\n****\n***\n**\n*");
+
+
+        System.out.println("-------------------------------------------------\n");
+
+        
+        // * LeftHalfPyramid
+        System.out.println("    *\n   **\n  ***\n ****\n*****");
+
+    }
+}
