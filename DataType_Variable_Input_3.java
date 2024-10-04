1c14bac491beacbdeed039b25443ad1533bb855d
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-04 20:00:59 +0530

    3-Data Types, Variables & Input

---
+++ DataType_Variable_Input_3.java
@@ -0,0 +1,97 @@
+public class DataType_Variable_Input_3 {
+
+public static void main(String args []){
+
+    // Variables are like containers used for storing data values.
+
+    int a=20;
+    System.out.println("int:- "+a);
+    
+    long b=20;
+    System.out.println("long:- "+b);
+    
+    char c='a';
+    System.out.println("char:- "+c);
+    
+    float d=20.2f;
+    System.out.println("float:- "+d);
+    
+    double e=20.3456;
+    System.out.println("double:- "+e);
+    
+    boolean f=true;
+    System.out.println("boolean:- "+f);
+
+    String g="Good Morning";
+    System.out.println("String:- "+g);
+    
+    
+    
+    System.out.println("");
+    System.out.println("-------------------Naming Conventions------------------ ");
+    System.out.println("");
+
+
+    System.out.println("camelCase:- "+ "myVariableName");
+    System.out.println("snake_case:- "+ "my_variable_name");
+    System.out.println("Kebab-case:- "+ "my-variable-name");
+    System.out.println("PascalCase:- "+ "MyVariableName");
+
+//!     Keep a Good and Short Name
+//*         • Choose names that are descriptive but not too long. It should
+//*            make it easy to understand the variable's purpose.
+//*         • Example: age, firstName, isMarried
+
+
+System.out.println("");
+System.out.println("------------------- Identifier Rules------------------ ");
+System.out.println("");
+
+//*     • The first character must be a letter or an underscore (_).
+//*     • Identifiers can only have alphanumeric characters and underscores.
+//*     • Identifiers cannot have spaces.
+//*     • Identifiers cannot be a keyword.
+//*     • Identifiers are case-sensitive.
+//*     • Identifiers cannot start with a digit.
+
+//*     • Example: myVariable, _myVariable, $myVariable, myVariable1
+//*     • Example: my Variable, 1myVariable, my-Variable, my.Variable
+
+//*     • There is no limit on the length of the identifier but it is advisable to use an optimum length of 4 – 15 letters only.
+
+
+
+    System.out.println("");
+    System.out.println("-------------------Escape Sequences------------------ ");
+    System.out.println("");
+
+    //!     \' - Single Quote'
+    System.out.println("Good \'Morning\'");
+
+    //!     \" - Double Quote"
+    System.out.println("Good \"Morning\"");
+
+    //!     \\ - Backslash
+    System.out.println("Good \\Morning\\");
+
+    //!     \n - New Line
+    System.out.println("Good \nMorning");
+
+    //!     \b - Backspace
+    System.out.println("Good\b Morning");
+
+    //!     \r - Carriage Return
+    System.out.println("Good \rMorning");
+
+    //!     \t - Tab
+    System.out.println("Good \tMorning");
+
+
+
+
+
+
+
+}
+
+}
