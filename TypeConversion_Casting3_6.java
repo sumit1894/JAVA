6f37fa459cd30c9096509d1287b7630b28ca14e6
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-05 20:27:43 +0530

    3.6-TypeConverstion

---
+++ TypeConversion_Casting3_6.java
@@ -0,0 +1,22 @@
+public class TypeConversion_Casting3_6 {
+    public static void main(String args[]){
+        
+        int num=100;
+        System.out.println(num);
+
+        //! Type Conversion
+
+        // * Implicit Type Conversion
+        // * byte -> short -> int -> long -> float -> double
+        
+        float num1=200;
+        System.out.println(num1);
+
+        // * Explicit Type Conversion
+        // * double -> float -> long -> int -> short -> byte
+
+        int num2=(int) 200.54;
+        System.out.println(num2);
+
+    }
+}
