8b3d9265e861612a1117213e5fd45ff4ae8e3003
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-05 19:40:45 +0530

    3-User_Input

---
+++ User_Input_3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class User_Input_3 {
+    public static void main(String args[]){
+
+        Scanner input_Scanner=new Scanner(System.in);
+        System.out.print("Enter the number of rows:- ");
+        String name = input_Scanner.nextLine();
+        System.out.println(name+",Good morning!");
+        System.out.print(name +", Enter your age also:- ");
+        int age =input_Scanner.nextInt();
+        System.out.println("Your age is "+age);
+        input_Scanner.close();// this is optional.
+
+
+    }
+}
