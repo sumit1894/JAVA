4a9125c970c03e0aa012b90b0e0be3737a6bf359
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-05 20:56:39 +0530

    Basic-Java

---
+++ Basic_java_1/Task_Pattern_2_2.java
@@ -0,0 +1,34 @@
+package Basic_java_1;
+public class Task_Pattern_2_2 {
+    public static void main(String args []){
+
+        System.out.println("-----------------RightHandPyramid---------------- ");
+
+
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * * ");
+        
+        System.out.println("-----------------ReverseHandPyramid------------------- ");
+        
+        System.out.println("* * * * * ");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+        
+
+        System.out.println("--------------------LeftHandPyramid---------------- ");
+        
+        
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * * ");
+
+
+    }
+}
