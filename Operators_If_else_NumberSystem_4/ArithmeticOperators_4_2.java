5a65904eacacd6b13423792b13dc1d8848a629d2
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-07 12:37:25 +0530

    4.2 Arithmetic Operators

---
+++ Operators_If_else_NumberSystem_4/ArithmeticOperators_4_2.java
@@ -0,0 +1,23 @@
+package Operators_If_else_NumberSystem_4;
+
+public class ArithmeticOperators_4_2 {
+    public static void main(String args[]){
+        //! Arithmetic Operators
+        //*   +, -, *, /, %
+
+        int a = 55, b = 20;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiply: " + (a * b));
+        System.out.println("division " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+        System.out.println("\n====================================\n");
+        double c = 10, d = 20;
+        System.out.println("Addition: " + (c + d));
+        System.out.println("Subtraction: " + (c - d));
+        System.out.println("Multiply: " + (c * d));
+        System.out.println("Multiply: " + (c * d));
+        System.out.println("division " + (c/ d));
+        System.out.println("Modulus: " + (c % d));
+    }
+}
