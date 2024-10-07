b435d9ff01148a666c83272a09a63b5215f23c03
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-07 12:18:08 +0530

    AssignmentOperator

---
+++ Operators_If_else_NumberSystem_4/AssignmentOperator_4_1.java
@@ -0,0 +1,17 @@
+package Operators_If_else_NumberSystem_4;
+
+public class AssignmentOperator_4_1 {
+    public static void main(String args[]) {
+        // Assigns the right-hand operand's value to the left-hand operand.
+        // Example: int a = 5;
+
+        int mynum=5;
+        System.out.println(mynum);
+
+        int anotherMynum=mynum;
+        System.out.println(anotherMynum);
+        
+
+        
+    }
+}
