55cf8df597faa1b9abd3fe2e31251d3ae0e1d862
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-07 13:55:55 +0530

    Arithmetic Operators

---
+++ Task/UserArithmeticOperators.java
@@ -0,0 +1,28 @@
+package Task;
+
+import java.util.Scanner;
+
+public class UserArithmeticOperators {
+    public static void main(String args[]) {
+        
+        //! Create a program that takes two numbers from the user and performs arithmetic operations on them.
+
+        Scanner Input=new Scanner(System.in);
+        System.out.print("Enter the value of a: ");
+        int a = Input.nextInt();
+        System.out.print("Enter the value of b: ");
+        int b = Input.nextInt();
+
+        System.out.println("a = " + a + " b = " + b);
+        System.out.println("a + b = " + (a+b));
+        System.out.println("a - b = " + (a-b));
+        System.out.println("a / b = " + (a/b));
+        System.out.println("a * b = " + (a*b));
+        System.out.println("a % b = " + (a%b));
+
+        Input.close();
+
+
+
+    }
+}
