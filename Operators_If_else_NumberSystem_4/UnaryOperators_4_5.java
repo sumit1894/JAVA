de3bb0f745fcea82f85a64edad372ddda780c66d
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-07 13:48:01 +0530

    4.5 Unary Operators

---
+++ Operators_If_else_NumberSystem_4/UnaryOperators_4_5.java
@@ -0,0 +1,41 @@
+package Operators_If_else_NumberSystem_4;
+
+public class UnaryOperators_4_5 {
+    public static void main(String args[]){
+
+        //! Unary Operators
+
+        //*  convert a positive number to a negative number.
+
+        int a = 5;
+        int b=-a;
+        System.out.println("b = -a: " + b);
+
+
+        //*  increment operator
+        int c = 5;
+        System.out.println("c++: " + c++); //5 //post increment
+        //6
+        System.out.println("++c: " + ++c);//7  //pre increment
+
+
+        //*  decrement operator
+        int d = 5;
+        System.out.println("d--: " + d--); //5  //post decrement
+        //4
+        System.out.println("--d: " + --d);//3   //pre decrement
+
+
+    
+
+
+
+
+
+
+
+
+
+
+    }
+}
