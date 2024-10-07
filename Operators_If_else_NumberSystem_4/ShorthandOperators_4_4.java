ff22d9a614248ce9118fc0ab40b4199b2c0538e2
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-07 13:12:01 +0530

    4.4 Shorthand Operators

---
+++ Operators_If_else_NumberSystem_4/ShorthandOperators_4_4.java
@@ -0,0 +1,23 @@
+package Operators_If_else_NumberSystem_4;
+
+public class ShorthandOperators_4_4 {
+    public static void main (String args[]){
+        //! Shorthand Operators
+        //*   +=, -=, *=, /=, %=
+
+        int a = 5, b = 10;
+        System.out.println("a = " + a + " b = " + b);
+        a += b;
+        System.out.println("a += b: " + a);
+        a -= b;
+        System.out.println("a -= b: " + a);
+        a *= b;
+        System.out.println("a *= b: " + a);
+        a/=b;
+        System.out.println("a /= b: " + a);
+        a%=b;
+        System.out.println("a %= b: " + a);
+
+
+    }
+}
