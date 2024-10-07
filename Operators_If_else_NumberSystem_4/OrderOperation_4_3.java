3a90f2e84981c02d7caf037dfc7d96783a48c25a
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-07 13:01:51 +0530

    4.3 Order of Operation

---
+++ Operators_If_else_NumberSystem_4/OrderOperation_4_3.java
@@ -0,0 +1,21 @@
+package Operators_If_else_NumberSystem_4;
+
+public class OrderOperation_4_3 {
+    
+    public static void main(String args[]){
+
+        //! Order of Operations
+
+        //* Parentheses
+        //* Exponents
+        //* Multiplication and Division
+        //* Addition and Subtraction
+
+        //todo  left to right if same precedence
+
+        int a = 5, b = 10, c = 15, d = 20;
+        System.out.println("a + b * c / d = " + (a + b * c / d));
+        
+
+    }
+}
