d5728f73231ef7031734990b0bfb2e72dbc00828
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-08 13:02:37 +0530

    odd or even

---
+++ Task/Odd_Even_16.java
@@ -0,0 +1,14 @@
+package Task;
+
+public class Odd_Even_16 {
+    public static void main(String args[]){
+        //! Create a program that determines if a number is odd or even.
+        int n=5;
+        if(n%2==0){
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
