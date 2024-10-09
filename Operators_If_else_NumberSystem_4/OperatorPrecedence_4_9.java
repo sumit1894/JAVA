df0d2dff1e0526e24a396e7d1ac5548178937623
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-09 17:11:09 +0530

    4.9 Operator Precedence

---
+++ Operators_If_else_NumberSystem_4/OperatorPrecedence_4_9.java
@@ -0,0 +1,34 @@
+package Operators_If_else_NumberSystem_4;
+
+public class OperatorPrecedence_4_9 {
+    public static void main(String[] args) {
+        
+//! Operator Precedence: Determines the evaluation order of operators in an expression based on their priority levels.
+
+
+//? Highest Priority: Postfix operators: [] . () ++ --
+//? Unary operators: ++ -- + - ~ !
+//? Multiplicative operators: * / %
+//? Additive operators: + -
+//? Shift operators: << >> >>>
+//? Relational operators: < > <= >= instanceof
+//? Equality operators: == !=
+//? Bitwise AND: &
+//? Bitwise XOR: ^
+//? Bitwise OR: |
+//? Logical AND: &&
+//? Logical OR: ||
+//? Ternary operator: ?:
+//? Assignment operators: = += -= *= /= %= &= ^= |= <<= >>= >>>=
+
+//! Associativity: Defines the order of operation for operators with the same precedence, usually left-to-right or right-to-left.
+
+//? Left-to-right: + - * / % << >> >>> & | && || = += -= *= /= %= &= ^= |= <<= >>= >>>=
+//? Right-to-left: ++ -- ! ~
+//? Right-to-left: ?: (Ternary operator)
+
+
+
+
+    }
+}
