0b7fee4e9c5a64ac4b53970a7da052535ad94f21
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-09 18:55:13 +0530

    Number System

---
+++ Operators_If_else_NumberSystem_4/NumberSystem_4_10.java
@@ -0,0 +1,30 @@
+package Operators_If_else_NumberSystem_4;
+
+public class NumberSystem_4_10 {
+    public static void main(String args[]) {
+
+        // ! Number System: A system of counting and arithmetic operations.
+        // ? Decimal Number System: Base 10, Digits: 0-9
+        // ? Binary Number System: Base 2, Digits: 0, 1
+        // ? Octal Number System: Base 8, Digits: 0-7
+        // ? Hexadecimal Number System: Base 16, Digits: 0-9, A-F
+
+        // ! Decimal Number System
+
+        // ? Decimal Number System is a base-10 system that has ten digits: 0, 1, 2, 3,4, 5, 6, 7, 8, 9.
+        // ? • The decimal number system is said to be of base, or radix, 10 because it uses 10 digits and the coefficients are multiplied by powers of 10.
+        // ? • This is the base that we often use in our day to day life.
+        // ? • Example: (7,392)10 , where 7,392 is a shorthand notation for what should be written as
+        // ? 7 * 103 + 3 * 102 + 9 * 101 + 2 * 100
+
+
+        // ! Binary Number System
+
+        // ? Binary Number System is a base-2 system that has two digits: 0, 1.
+        // ? • The binary number system is said to be of base, or radix, 2 because it uses 2 digits and the coefficients are multiplied by powers of 2.
+        // ? • This is the base that is used in digital systems.
+        // ? • Example: (1011)2 , where 1011 is a shorthand notation for what should be written as
+        // ? 1 * 23 + 0 * 22 + 1 * 21 + 1 * 20 = 8 + 0 + 2 + 1 = 11
+
+    }
+}
