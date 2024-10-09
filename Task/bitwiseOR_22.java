45495f6b7f3c5267ee4047de99477d6919bf13de
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-09 19:51:28 +0530

    bitwise OR

---
+++ Task/bitwiseOR_22.java
@@ -0,0 +1,17 @@
+package Task;
+
+import java.util.Scanner;
+
+public class bitwiseOR_22 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Enter a number ");
+        int inp1=input.nextInt();
+        System.out.print("Enter a number ");
+        int inp2=input.nextInt();
+
+        System.out.println(inp1 | inp2);
+
+    }
+}
