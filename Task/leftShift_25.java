ecb716c206d1e981dc87efb6ed57a282a2f68a48
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-09 20:06:48 +0530

    left shift operator

---
+++ Task/leftShift_25.java
@@ -0,0 +1,15 @@
+package Task;
+
+import java.util.Scanner;
+
+public class leftShift_25 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Enter a number ");
+        int inp1=input.nextInt();
+
+        System.out.println(inp1<<1);
+        input.close();
+    }
+}
