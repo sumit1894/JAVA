3eab044b450fbd4c31bff07eeddc3232549be858
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-11 13:48:02 +0530

    5.1 Comments

---
+++ WhileLoop_Methods_Arrays_5/Comments_1.java
@@ -0,0 +1,26 @@
+package WhileLoop_Methods_Arrays_5;
+
+/**
+ * this is my main java docs
+ * @param args
+ */
+
+public class Comments_1 {
+    public static void main(String[] args) {
+        // ? 1. Used to add notes in Java code
+        // ? 2. Not displayed on the web page
+        // ? 3. Helpful for code organization
+
+        // ? 4. Syntax:
+        // ! 1. Single Line: //
+        // ! 2. Multi Line: /* */
+        // ! 3. Java Docs: /** */
+
+        // Single Line Comment
+
+        /*
+         * Multi Line Comment
+         */
+
+    }
+}
