b08c58971107cff9c98c2aabc547e5bc8a6ed4b8
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-11 14:08:08 +0530

    5.2 Loop

---
+++ WhileLoop_Methods_Arrays_5/Loop_2.java
@@ -0,0 +1,29 @@
+package WhileLoop_Methods_Arrays_5;
+
+public class Loop_2 {
+    public static void main(String[] args) {
+        // 1. Code that runs multiple times based on a condition.
+        // 2. Repeated execution of code.
+        // 3. Loops automate repetitive tasks.
+        // 4. Types of Loops: while, for, do-while.
+        // 5. Iterations: Number of times the loop runs.
+
+        // While Loop +ve
+        int a=1;
+    
+        while(a<=10){
+            System.out.println(a);
+            a++;
+        }
+        System.out.println("\n");
+        // While Loop -ve
+        int b=10;
+        while(b>=0){
+            System.out.println(b);
+            b--;
+        }
+
+
+
+    }
+}
