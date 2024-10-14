f0f73d495491127e3b18a1ef200adba78f6c7d39
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-14 14:31:52 +0530

    maximum and minimum element

---
+++ Task/maximumAndMinimum_42.java
@@ -0,0 +1,53 @@
+package Task;
+
+public class maximumAndMinimum_42 {
+    public static void main(String[] args) {
+
+        int[] arr=ArrayUtility_40.inputArray();
+
+        int maximun=maximun(arr);
+        System.out.println("Maximum number in an array is: "+maximun);
+
+        int minimum=minimum(arr);
+        System.out.println("mininum number in an array is: "+minimum);
+
+
+
+
+    }
+
+    public static int maximun(int[] arr){
+        int i=1;
+        int max=arr[0];
+        if(arr.length==0){
+            return Integer.MIN_VALUE;
+        }
+
+        while (i<arr.length) {
+            if(max<arr[i]){
+                max=arr[i];
+            }
+            i++;
+        }
+        return max;
+    }
+
+    public static int minimum(int[] arr){
+        int i=0;
+        int min=Integer.MAX_VALUE;
+
+        if(arr.length==0){
+            return Integer.MIN_VALUE;
+        }
+        
+        while(i<arr.length){
+
+            if(min>arr[i]){
+                min=arr[i];
+            }
+            i++;
+
+        }
+        return min;
+    }
+}
