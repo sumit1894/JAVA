980678d6f3527f1f93faffac1fb7a9f73f8b6243
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-15 11:31:07 +0530

    Array input & output

--- Task/ArrayUtility_40.java
+++ Task/ArrayUtility_40.java
@@ -21,6 +21,30 @@ public class ArrayUtility_40 {
         return nums;
     }
 
+    public static int[][] input2DArray(){
+
+        Scanner inp=new Scanner(System.in);
+        System.out.print("Pleased Enter the Row of an Array: ");
+        int Row =inp.nextInt();
+        System.out.print("Pleased Enter the Coloumns of an Array: ");
+        int Coloumns =inp.nextInt();
+        
+        int[][] nums=new int[Row][Coloumns];
+        
+        int i=0;
+        while(i<Row){
+            int j=0;
+            while(j<Coloumns){
+                System.out.print("Enter " +(i+1)+ " row "+ (j+1)+ " coloum: ");
+                nums[i][j]=inp.nextInt();
+                j++;
+            }
+            i++;
+            
+        }
+        return nums;
+    }
+
 
     public static void diaplayArray(int[] nums){
 
