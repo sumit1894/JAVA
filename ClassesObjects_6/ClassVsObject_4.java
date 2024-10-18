165884e5ec4c4611bae1fee7625500ec0dda45d9
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-18 23:17:17 +0530

    6.4 Class vs Object

---
+++ ClassesObjects_6/ClassVsObject_4.java
@@ -0,0 +1,54 @@
+package ClassesObjects_6;
+
+public class ClassVsObject_4 {
+    public static void main(String[] args) {
+        
+        //! Class vs Object
+        
+        //* Class is a blueprint; Objects are real values in memory.
+        //* Class is a template; Objects are instances of the class.
+        //* Class is a user-defined data type; Objects are variables of the class type.
+        //* Class does not occupy memory space; Objects occupy memory space.
+        //* Class does not have a memory address; Objects have a memory address.
+        //* Class does not have a state; Objects have a state.
+        //* Class does not have a life cycle; Objects have a life cycle.
+        //* Class does not have a lifetime; Objects have a lifetime.
+        //* Class does not have a value; Objects have a value.
+        //* Class does not have a reference; Objects have a reference.
+        //* Class does not have a behavior; Objects have a behavior.
+        //* Class does not have a scope; Objects have a scope.
+
+        //! Example
+        //todo class: car
+        //todo object: car1, car2, car3
+        //todo car: blueprint
+        //todo car1, car2, car3: real cars
+
+        //? class: car // properties and methods of car
+        // * property
+        // color
+        // price
+        // CurrentFuleInLitres
+        // NumberOfSeats
+
+        // * methods
+        // start()
+        // drive()
+        // addFuel()
+        // getCurrentFuel()
+
+        //? object: car1 // real car
+        // * color: red
+        // * price: 10000
+        // * CurrentFuleInLitres: 10
+        // * NumberOfSeats: 4
+
+        //* methods
+        // start(): Car is starting
+        // drive(): Car is driving
+        // addFuel(): 10
+        // getCurrentFuel(): 10
+        
+
+    }
+}
