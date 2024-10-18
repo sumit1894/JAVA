f2b15d2f583a94b2c1a973a768d48e99a1741b0a
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-18 20:41:06 +0530

    6.2 Instance Variables and Methods

---
+++ ClassesObjects_6/Instance_Variables_Methods_2.java
@@ -0,0 +1,23 @@
+package ClassesObjects_6;
+
+public class Instance_Variables_Methods_2 {
+    public static void main(String[] args) {
+        
+        // ! Class 
+        // ? Example Cat 
+        //todo  1. properties/attributes/fields/instance variables
+        //todo  2. methods/behaviors
+
+        // ! Properties/Attributes/Fields/Instance Variables
+        // ? Example: Cat
+        // ? Properties: name, color, breed, age, etc
+
+        // ! Methods/Behaviors
+        // ? Example: Cat
+        // ? Methods: eat(), sleep(), run(), etc
+
+        
+
+
+    }
+}
