7db59421d98e89e4489bf4b8494e093db9b0d2e2
Sumit Prasad <sumit.study1894@gmail.com>
2024-10-18 19:09:29 +0530

    6.1 Process vs Object Oriented

---
+++ ClassesObjects_6/ProcessVsObjectOriented_1.java
@@ -0,0 +1,28 @@
+package ClassesObjects_6;
+
+public class ProcessVsObjectOriented_1 {
+    public static void main(String[] args) {
+
+        // ! Process Orienter Programming is a traditional way of programming where we
+        // write code in a sequence of steps.
+        // example of process oriented programming is C programming language.
+        // ? Widraw , deposit, check balance, etc are the examples of process oriented
+        // programming.
+
+
+        // ! Object Oriented Programming is a programming paradigm that is based on the
+        // concept of objects.
+        // Objects are the instance of classes. In OOP, we write code in terms of
+        // objects.
+        // ? Customer, Account, Bank, etc are the examples of object oriented
+        // programming.
+
+        
+        // Process Oriented Programming
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum of a and b is: " + sum);
+
+    }
+}
